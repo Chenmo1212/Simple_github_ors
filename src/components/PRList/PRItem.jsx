efda0c6e15006323f6c8ed0ca4c3aa28c52f19f4
chenmo <chenmo1212@qq.com>
2023-10-18 22:52:59 +0100

    style: Add ellipses to long comments

--- src/components/PRList/PRItem.jsx
+++ src/components/PRList/PRItem.jsx
@@ -6,13 +6,20 @@ const PRItem = ({title, url, author, authorUrl, comments}) => {
     return (
         <div className="pr-item">
             <div className="pr-item-header">
-                <h3 className="pr-title" title={title || 'Title Not Available'}><a href={url}>{title || 'Title Not Available'}</a></h3>
-                <p className="pr-author">Author: <a href={authorUrl}>{author || 'Author Not Available'}</a></p>
+                <h3 className="pr-title" title={title || 'Title Not Available'}>
+                    <a href={url}>{title || 'Title Not Available'}</a>
+                </h3>
+                <p className="pr-author">Created by <a href={authorUrl}>{author || 'Author Not Available'}</a></p>
             </div>
-            <p className="pr-item-comment">
-                <img src={commentSvg} className="comment-icon" alt="comment-icon"/>
-                <span className="comment-amount">{comments || 0}</span>
-            </p>
+            {
+                comments ?
+                    (<p className="pr-item-comment">
+                        <img src={commentSvg} className="comment-icon" alt="comment-icon"/>
+                        <span className="comment-amount" title={comments}>{comments > 99 ? "99+": comments || 0}</span>
+                    </p>)
+                    : ""
+            }
+
         </div>
     );
 };
