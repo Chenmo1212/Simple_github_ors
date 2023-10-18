0bfa45bc46f8ba7b08d8f849783a47d0a600f915
chenmo <chenmo1212@qq.com>
2023-10-18 20:50:51 +0100

    feat: add PRItem component

---
+++ src/components/PRList/PRItem.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import "./PRItem.css"
+import commentSvg from "../../assets/images/comment.svg"
+
+const PRItem = ({title, url, author, authorUrl, comments}) => {
+    return (
+        <div className="pr-item">
+            <div className="pr-item-header">
+                <h3 className="pr-title"><a href={url}>{title || 'Title Not Available'}</a></h3>
+                <p className="pr-author">Author: <a href={authorUrl}>{author || 'Author Not Available'}</a></p>
+            </div>
+            <p className="pr-item-comment">
+                <img src={commentSvg} className="comment-icon" alt="comment-icon"/>
+                <span className="comment-amount">{comments || 0}</span>
+            </p>
+        </div>
+    );
+};
+export default PRItem;
--- src/components/PRList/PRList.jsx
+++ src/components/PRList/PRList.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { fetchPullRequests } from '../../axios/api';
+import PRItem from "./PRItem";
+import './PRList.css';
 
 const PRList = () => {
     const [prs, setPRs] = useState([]);
@@ -8,7 +10,16 @@ const PRList = () => {
         // Fetch PR data from the GitHub API
         fetchPullRequests()
             .then((res) => {
-                setPRs(res.data);
+                const prData = res.data.map((pr, index) => ({
+                    id: pr.id || index,
+                    title: pr.title || 'Title Not Available',
+                    url: pr.url || '/',
+                    author: pr.user['login'] || 'Author Not Available',
+                    authorUrl: pr.user['html_url'] || '/',
+                    comments: pr.comments || 0
+                }));
+
+                setPRs(prData);
             })
             .catch((err) => {
                 console.log(err)
@@ -19,14 +30,20 @@ const PRList = () => {
     }, []);
 
     return (
-        <div>
+        <div className="pr-list">
             <h2>Pull Requests</h2>
             <ul>
                 {prs.map((pr) => (
                     <li key={pr.id}>
-                        {pr.title}
-                    </li>
-                ))}
+                        <PRItem
+                            title={pr.title}
+                            author={pr.author}
+                            url={pr.url}
+                            authorUrl={pr.authorUrl}
+                            comments={pr.comments || 0}
+                        />
+                    </li>))
+                }
             </ul>
         </div>
     );
